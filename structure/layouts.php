3c49a5e04c73250ebfcd969d2209cab6bcc0af0f
IvanMusila <ivan.musila@strathmore.edu>
2024-10-07 09:36:40 +0300

    Layout changes

--- structure/layouts.php
+++ structure/layouts.php
@@ -22,10 +22,22 @@
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Sign Up</title>
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+            <link href="../css/bootstrap.min.css" rel="stylesheet">
         </head>
         <body>
         <?php
+        }
+
+        public function banner(){
+            ?>
+              <div class="p-3 mb-4 bg-body-tertiary rounded-3">
+                <div class="container-fluid py-2">
+                  <h1 class="display-5 fw-bold">Assignemnt II</h1>
+                  <p class="col-md-8 fs-4">This is an individual assignment. We assume that you already know how to set up a localhost server(s): & MySQL database and Apache web servers.<br>If not, your instructor will assist you to set it up.  </p>
+                  <button class="btn btn-primary btn-lg" type="button">Example button</button>
+                </div>
+              </div>
+            <?php
         }
 
         public function footer(){
