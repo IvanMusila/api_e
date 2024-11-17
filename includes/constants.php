fe342426be321abef272f768322c09dfb7454e59
IvanMusila <ivan.musila@strathmore.edu>
2024-11-18 00:17:15 +0300

    changed port number

--- includes/constants.php
+++ includes/constants.php
@@ -1,7 +1,7 @@
 <?php
 define('DBTYPE', 'PDO');
 define('HOSTNAME', 'localhost');
-define('DBPORT', '3307');
+define('DBPORT', '3306');
 define('HOSTUSER', 'root');
 define('HOSTPASS', 'Muvane2005@');
 define('DBNAME', 'ics_e');
