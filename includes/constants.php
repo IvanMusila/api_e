f423aef2ce90491df8fb4ecd3ea3444b02342344
IvanMusila <ivan.musila@strathmore.edu>
2024-11-18 09:07:25 +0300

    Additional phpmailer constants

--- includes/constants.php
+++ includes/constants.php
@@ -5,3 +5,10 @@ define('DBPORT', '3306');
 define('HOSTUSER', 'root');
 define('HOSTPASS', 'Muvane2005@');
 define('DBNAME', 'ics_e');
+
+$protocol = isset($_SERVER['HTTPS']) && 
+$_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://';
+$base_url = $protocol . $_SERVER['HTTP_HOST'] . '/';
+$conf['verification_code'] = rand(10000,99999);
+$conf['site_initials'] = "ICS 2024";
+$conf['site_url'] = "$base_url/". DBNAME;
